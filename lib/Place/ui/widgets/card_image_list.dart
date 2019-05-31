f3f1e0f48fa080baed2de94bd0ac48c09e70401b
José M. Chavarría <mauxchg11@gmail.com>
2019-05-30 23:42:47 -0600

    agregando generic-bloc-provider al proyecto e iniciandolo en place y user

--- lib/Place/ui/widgets/card_image_list.dart
+++ lib/Place/ui/widgets/card_image_list.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+      import 'package:flutter/material.dart';
 import 'package:platzi_trips_app/Place/ui/widgets/card_image.dart';
 
 class CardImageList extends StatelessWidget {
