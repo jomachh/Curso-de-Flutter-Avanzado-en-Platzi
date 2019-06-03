c37fe025a8803a9f7ee6f735059364c6f07731d2
José M. Chavarría <mauxchg11@gmail.com>
2019-06-03 12:21:59 -0600

    Implementado con exito el inicio de sesion con Google en Android y iOS

--- build/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: /Users/mauricio/Documents/Flutter/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
--- lib/User/bloc/bloc_user.dart
+++ lib/User/bloc/bloc_user.dart
@@ -1,9 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/repository/auth_repository.dart';
 
 class UserBloc implements Bloc {
+  final _auth_repository = AuthRepository();
+
+  Stream<FirebaseUser> streamFirebase =
+      FirebaseAuth.instance.onAuthStateChanged;
+  Stream<FirebaseUser> get authStatus =>
+      streamFirebase; //retorna el estado de la sesión
+
+  //Casos de uso
+  //1. SignIn a la aplicación con Google
+  Future<FirebaseUser> signIn() {
+    return _auth_repository.signinFirebase();
+  }
 
   @override
-  void dispose() {
-
-  }
+  void dispose() {}
 }
